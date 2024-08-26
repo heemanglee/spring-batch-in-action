70dea901005ebd8c414b5fb2521b039f45578dd6
w1shope <wishhopelee@gmail.com>
2024-08-26 11:45:51 +0900

    중복(Job 이름, JobParameters)된 JOB_INSTANCE를 사용할 수 없다.

--- src/main/java/com/example/spring_batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/spring_batch/job/SimpleJobConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -26,15 +27,16 @@ public class SimpleJobConfiguration {
     @Bean
     public Job job() {
         return new JobBuilder("simpleJob1", jobRepository)
-            .start(simpleStep1())
+//            .start(simpleStep1())
+            .start(simpleStep3())
+            .next(simpleStep2(null))
             .build();
     }
 
     // Step이란 Job을 구성하는 하나의 작업 단위이다. -> 배치 작업 내에서 개별 작업 단위를 의미한다.
     // Step은 Tasklet을 실행한다.
-    @Bean
-    @JobScope
-    public Step simpleStep1() { // import org.springframework.beans.factory.annotation.Value
+//    @Bean
+    public Step simpleStep1() {
         return new StepBuilder("simplestep1", jobRepository)
             // Tasklet은 Step에서 실행되는 작업 단위이다.
             // 이 작업이 끝나면 Step이 끝나고, Job의 다음 단계로 넘어간다.
@@ -42,6 +44,28 @@ public class SimpleJobConfiguration {
                 log.info(">>>>> This is step1");
                 return RepeatStatus.FINISHED;
             }), transactionManager).build();
+    }
+
+    @Bean
+    @JobScope // JobParameters를 사용하기 위해서 @JobScope를 사용한다.
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return new StepBuilder("simplestep2", jobRepository)
+            .tasklet(((contribution, chunkContext) -> {
+                log.info(">>>>> This is step2");
+                log.info(">>>>> requestDate = {}", requestDate);
+                return RepeatStatus.FINISHED;
+            }), transactionManager).build();
+    }
+
+    @Bean
+    public Step simpleStep3() {
+        return new StepBuilder("simplestep3", jobRepository)
+            .tasklet((contribution, chunkContext) -> {
+//                throw new IllegalArgumentException("step3에서 예외 발생");
+
+                log.info(">>>>> 예외가 아닌, 정상적으로 배치 작업을 수행한다.");
+                return RepeatStatus.FINISHED;
+            }, transactionManager).build();
     }
 
 }
